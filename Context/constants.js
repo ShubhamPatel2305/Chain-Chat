02a13889546c450c9f2ec9a67585b77210d4c46a
PATEL MOKSH <patelmoksh@PATELs-MacBook-Air.local>
2023-02-04 17:01:11 +0530

    Basic Chat DApp on 5IRE

---
+++ Context/constants.js
@@ -0,0 +1,7 @@
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+import chatApp from "./ChatApp.json";
+
+export const ChatAppAddress = "0xf90E7B80e8EdB1D375f8764152Ff4A998a1e86ae";
+
+export const ChatAppABI = chatApp.abi;
+  
